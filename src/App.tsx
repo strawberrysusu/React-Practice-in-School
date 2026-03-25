6f06dd5bbbc2d11249f4508ac3e691b84207fcc9
strawberrysusu <qjwkr15@gmail.com>
2026-03-25 10:31:30 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import './App.css'
+import Profile from './Profile'
+
+export default function App() {
+  return <Profile />
+}
---
+++ src/Profile.tsx
@@ -0,0 +1,6 @@
+import reactLogo from './assets/react.svg'
+
+export default function Profile() {
+    
+  return <img src={reactLogo} className="logo react" alt="React logo" />
+}
